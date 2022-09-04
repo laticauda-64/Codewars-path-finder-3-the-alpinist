9ef83953c3cc4f418d5a866121c449ee54ad4fa1
YoannBDL <yoannb64@gmail.com>
2022-09-04 16:38:13 +0200

    Setting up Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,24 +5,51 @@ import org.example.foo.Finder;
 public class Main {
     public static void main(String[] args) {
 
-        String a =  ".W.\n"+
-                    ".W.\n"+
-                    "...",
+     String     a = "000\n"+
+                    "000\n"+
+                    "000",
 
-        b = ".W.\n"+
-            ".W.\n"+
-            "W..",
+                b = "010\n"+
+                    "010\n"+
+                    "010",
 
-        d = "..W...\n"+
-            "..W...\n"+
-            "WW....\n"+
-            "WWWWWW\n"+
-            ".....W\n"+
-            "...W..";
+                c = "010\n"+
+                    "101\n"+
+                    "010",
 
-//        System.out.println(Finder.pathFinder(a));
+                d = "0707\n"+
+                    "7070\n"+
+                    "0707\n"+
+                    "7070",
+
+                e = "700000\n"+
+                    "077770\n"+
+                    "077770\n"+
+                    "077770\n"+
+                    "077770\n"+
+                    "000007",
+
+                f = "777000\n"+
+                    "007000\n"+
+                    "007000\n"+
+                    "007000\n"+
+                    "007000\n"+
+                    "007777",
+
+                g = "000000\n"+
+                    "000000\n"+
+                    "000000\n"+
+                    "000010\n"+
+                    "000109\n"+
+                    "001010";
+
+        System.out.println(Finder.pathFinder(a));
         System.out.println(Finder.pathFinder(b));
-//        System.out.println(Finder.pathFinder(d));
+        System.out.println(Finder.pathFinder(c));
+        System.out.println(Finder.pathFinder(d));
+        System.out.println(Finder.pathFinder(e));
+        System.out.println(Finder.pathFinder(f));
+        System.out.println(Finder.pathFinder(g));
 
     }
 }
